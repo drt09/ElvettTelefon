13a060cc3345cba64779ef7c0c0f73041e61a94f
Frigyes Elemér <125876771+drt09@users.noreply.github.com>
2023-05-15 14:39:22 +0200

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
-var NevekListája = []
-var rovártrue = document.getElementById("rovás").checked
-var nev = document.getElementById("név").innerHTML
-var valasztas = document.getElementById("választás").value
+var szam = 1;
 
 function Fogykos()
 {
-    NevekListája.push(nev)
-    document.getElementById("kiir").innerHTML = NevekListája
+    var rovártrue = document.getElementById("rovás")
+    var nev = document.getElementById("név").value
+    var valasztas = document.getElementById("választás").value
+    if (rovártrue.checked)
+    {
+        
+        document.getElementById("kiir").innerHTML += '<option onclick="torol()" id="'+szam+'">'+nev+" "+valasztas+" !!!"+'</option>'
+        szam++
+    } else
+    {
+        document.getElementById("kiir").innerHTML += '<option onclick="torol()" id="'+szam+'">'+nev+" "+valasztas+'</option>'
+        szam++
+    }
+}
+
+function torol()
+{
+    document.getElementById("kiir").remove(document.getElementById("kiir").selectedIndex)
 }
